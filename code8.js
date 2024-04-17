11d85101608975237bbbee0d87114326c5719425
NikuTank <76245195+NikuTank@users.noreply.github.com>
2024-04-17 08:23:50 +0300

    Update code8.js

--- code8.js
+++ code8.js
@@ -9,6 +9,7 @@ function Task_8(){
     const poet = ["Пушкин", "Чехов", "Толстой", "Лермонтов", "Есенин", "Гоголь", "Маяковский"]
     let a = Math.floor(Math.random() * poet.length);
     let b = Math.floor(Math.random() * poet.length);
+    let c = a+"|"+b;
     while (a === b) {
     randomIndex2 = Math.floor(Math.random() * poet.length);
     A = getRandomInt(100,4000);
@@ -16,7 +17,9 @@ function Task_8(){
     A = getRandomInt(100,4000);
     Condition = "В языке запросов поискового сервера для обозначения логической операции «ИЛИ» используется символ «|», а для обозначения логической операции «И»  — символ «&». <br> В таблице приведены запросы и количество найденных по ним страниц некоторого сегмента сети Интернет."
     document.getElementById("Part_3").innerHTML = Condition;
-    document.getElementById("1").innerHTML = A;
+    document.getElementById("1t").innerHTML = a;
+    document.getElementById("2t").innerHTML = b;
+    document.getElementById("3t").innerHTML = c;
     document.getElementById("2").innerHTML = B;
     document.getElementById("3").innerHTML = C;
     document.getElementById("Otvet").innerHTML = A;
