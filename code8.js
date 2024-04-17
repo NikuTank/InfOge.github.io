2c8c7ba88ae972b25189165bf0704857fa2f0654
NikuTank <76245195+NikuTank@users.noreply.github.com>
2024-04-17 09:37:47 +0300

    Update code8.js

--- code8.js
+++ code8.js
@@ -5,6 +5,7 @@ function getRandomInt(min, max) {
 }
 let A = 0
 let B = 0
+let C = 0
 function Task_8(){
     // const poet = ["Пушкин", "Чехов", "Толстой", "Лермонтов", "Есенин", "Гоголь", "Маяковский"];
     // let a = Math.floor(Math.random() * poet.length);
@@ -12,16 +13,19 @@ function Task_8(){
     // // let c = a+"|"+b;
     // while (a === b) {
     // randomIndex2 = Math.floor(Math.random() * poet.length);
-    A = getRandomInt(100,4000);
-    B = getRandomInt(100,4000);
-    A = getRandomInt(100,4000);
+    A = getRandomInt(1,40);
+    B = getRandomInt(1,40);
+    C = getRandomInt(1,A)*100;
+    A = A * 100;
+    B = B * 100;
     Condition = "В языке запросов поискового сервера для обозначения логической операции «ИЛИ» используется символ «|», а для обозначения логической операции «И»  — символ «&». <br> В таблице приведены запросы и количество найденных по ним страниц некоторого сегмента сети Интернет."
     document.getElementById("Part_3").innerHTML = Condition;
     // document.getElementById("1t").innerHTML = a;
     // document.getElementById("2t").innerHTML = b;
     // document.getElementById("3t").innerHTML = c;
-    // document.getElementById("2").innerHTML = B;
-    // document.getElementById("3").innerHTML = C;
+    document.getElementById("2").innerHTML = A;
+    document.getElementById("2").innerHTML = B;
+    document.getElementById("3").innerHTML = C;
     // document.getElementById("Otvet").innerHTML = A;
 }
 function Answer() {
