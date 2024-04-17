3c38030e251fb419adaa4b340510c598d25c76bc
NikuTank <76245195+NikuTank@users.noreply.github.com>
2024-04-17 09:29:20 +0300

    Update code8.js

--- code8.js
+++ code8.js
@@ -6,23 +6,23 @@ function getRandomInt(min, max) {
 let A = 0
 let B = 0
 function Task_8(){
-    const poet = ["Пушкин", "Чехов", "Толстой", "Лермонтов", "Есенин", "Гоголь", "Маяковский"];
-    let a = Math.floor(Math.random() * poet.length);
-    let b = Math.floor(Math.random() * poet.length);
-    // let c = a+"|"+b;
-    while (a === b) {
-    randomIndex2 = Math.floor(Math.random() * poet.length);
+    // const poet = ["Пушкин", "Чехов", "Толстой", "Лермонтов", "Есенин", "Гоголь", "Маяковский"];
+    // let a = Math.floor(Math.random() * poet.length);
+    // let b = Math.floor(Math.random() * poet.length);
+    // // let c = a+"|"+b;
+    // while (a === b) {
+    // randomIndex2 = Math.floor(Math.random() * poet.length);
     A = getRandomInt(100,4000);
     B = getRandomInt(100,4000);
     A = getRandomInt(100,4000);
     Condition = "В языке запросов поискового сервера для обозначения логической операции «ИЛИ» используется символ «|», а для обозначения логической операции «И»  — символ «&». <br> В таблице приведены запросы и количество найденных по ним страниц некоторого сегмента сети Интернет."
-    document.getElementById("Part_3").innerHTML = Condition;
-    document.getElementById("1t").innerHTML = a;
-    document.getElementById("2t").innerHTML = b;
-    document.getElementById("3t").innerHTML = c;
-    document.getElementById("2").innerHTML = B;
-    document.getElementById("3").innerHTML = C;
-    document.getElementById("Otvet").innerHTML = A;
+    // document.getElementById("Part_3").innerHTML = Condition;
+    // document.getElementById("1t").innerHTML = a;
+    // document.getElementById("2t").innerHTML = b;
+    // document.getElementById("3t").innerHTML = c;
+    // document.getElementById("2").innerHTML = B;
+    // document.getElementById("3").innerHTML = C;
+    // document.getElementById("Otvet").innerHTML = A;
 }
 function Answer() {
     if (document.getElementById("InpOtvet").value == document.getElementById('Otvet').innerHTML) {
