43945d836a2833d499957d248882a9cc7ce9f23d
NikuTank <76245195+NikuTank@users.noreply.github.com>
2024-04-17 09:57:56 +0300

    Update code8.js

--- code8.js
+++ code8.js
@@ -8,18 +8,20 @@ let B = 0
 let C = 0
 function Task_8(){
     const poet = ["Пушкин", "Чехов", "Толстой", "Лермонтов", "Есенин", "Гоголь", "Маяковский"];
-    let a = Math.floor(Math.random() * poet.length);
-    let b = Math.floor(Math.random() * poet.length);
-    // // let c = a+"|"+b;
-    while (a === b) {
-        b = Math.floor(Math.random() * poet.length);}
-    A = getRandomInt(5,40);
-    B = getRandomInt(5,40);
-    if(A<B){
-        let min = A;
-    }else{
-        let min = B;
-    }
+    // let a = Math.floor(Math.random() * poet.length);
+    // let b = Math.floor(Math.random() * poet.length);
+    // // // let c = a+"|"+b;
+    // while (a === b) {
+    //     b = Math.floor(Math.random() * poet.length);}
+    // A = getRandomInt(5,40);
+    // B = getRandomInt(5,40);
+    // if(A<B){
+    //     let min = A;
+    // }else{
+    //     let min = B;
+    // }
+    let a = "Пушкин";
+    let b = "Толстой";
     C = getRandomInt(2,(A-1))*100;
     A = A * 100;
     min = min * 100;
